7623c5c429bfdcdb5d0d2ba1b50a72729d6f825e
marcoscoutinhodev@outlook.com <marcoscoutinhodev@outlook.com>
2023-10-02 18:10:12 -0300

    feat: [user] add register use case

---
+++ internal/domain/user/interface.go
@@ -0,0 +1,44 @@
+package user
+
+import (
+	"context"
+
+	"github.com/marcoscoutinhodev/mv_chat/internal/entity"
+)
+
+type RegisterInput struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type AuthInput struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type Output struct {
+	StatusCode int         `json:"-"`
+	Data       interface{} `json:"data,omitempty"`
+	Error      interface{} `json:"error,omitempty"`
+}
+
+type UseCaseInterface interface {
+	Register(ctx context.Context, input *RegisterInput) error
+	Auth(ctx context.Context, input *AuthInput) (*entity.User, error)
+}
+
+type Repository interface {
+	Find(ctx context.Context, id string) (*entity.User, error)
+	FindByEmail(ctx context.Context, email string) (*entity.User, error)
+	Store(ctx context.Context, user *entity.User, fn func() error) error
+}
+
+type Hasher interface {
+	Generate(plaintext string) (string, error)
+	Compare(hash, plaintext string) error
+}
+
+type Queue interface {
+	RegisterNotification(ctx context.Context, user *entity.User) error
+}
---
+++ internal/domain/user/usecase.go
@@ -0,0 +1,71 @@
+package user
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	"github.com/marcoscoutinhodev/mv_chat/internal/entity"
+)
+
+type UseCase struct {
+	hasher     Hasher
+	repository Repository
+	queue      Queue
+}
+
+func NewUseCase(hasher Hasher, repository Repository, queue Queue) *UseCase {
+	return &UseCase{
+		hasher:     hasher,
+		repository: repository,
+		queue:      queue,
+	}
+}
+
+func (s UseCase) Register(ctx context.Context, input *RegisterInput) *Output {
+	u, err := s.repository.FindByEmail(ctx, input.Email)
+	if err != nil {
+		return &Output{
+			StatusCode: http.StatusInternalServerError,
+			Error:      err,
+		}
+	}
+
+	if u != nil {
+		return &Output{
+			StatusCode: http.StatusConflict,
+			Error:      errors.New("email is already registered"),
+		}
+	}
+
+	hashedPassword, err := s.hasher.Generate(input.Password)
+	if err != nil {
+		return &Output{
+			StatusCode: http.StatusInternalServerError,
+			Error:      err,
+		}
+	}
+
+	user := entity.NewUser(input.Name, input.Email, hashedPassword)
+
+	// this function will ensure that the user who has just been stored in the database
+	// will receive the verification email, otherwise it will return an error and the
+	// user will not be stored in the database
+	if err := s.repository.Store(ctx, user, func() error {
+		if err := s.queue.RegisterNotification(ctx, user); err != nil {
+			return err
+		}
+
+		return nil
+	}); err != nil {
+		return &Output{
+			StatusCode: http.StatusInternalServerError,
+			Error:      err,
+		}
+	}
+
+	return &Output{
+		StatusCode: http.StatusCreated,
+		Data:       "check your inbox to verify your email and activate your account",
+	}
+}
