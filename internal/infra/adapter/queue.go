50fabccbc2769f7a58d03400b21b34e094c27a66
marcoscoutinhodev@outlook.com <marcoscoutinhodev@outlook.com>
2023-10-03 12:23:00 -0300

    feat: [queue] add forgotten-password-notification adapter

--- internal/infra/adapter/queue.go
+++ internal/infra/adapter/queue.go
@@ -10,16 +10,24 @@ import (
 )
 
 type Queue struct {
-	amqp                         amqp.AMQP
-	registerNotificationQueue    string
-	registerNotificationExchange string
+	amqp                                  amqp.AMQP
+	registerNotificationKey               string
+	registerNotificationQueue             string
+	registerNotificationExchange          string
+	forgottenPasswordNotificationKey      string
+	forgottenPasswordNotificationQueue    string
+	forgottenPasswordNotificationExchange string
 }
 
 func NewQueue() *Queue {
 	return &Queue{
-		amqp:                         *amqp.NewAMQP(),
-		registerNotificationQueue:    os.Getenv("REGISTER_NOTIFICATION_QUEUE"),
-		registerNotificationExchange: os.Getenv("REGISTER_NOTIFICATION_EXCHANGE"),
+		amqp:                                  *amqp.NewAMQP(),
+		registerNotificationQueue:             os.Getenv("REGISTER_NOTIFICATION_QUEUE"),
+		registerNotificationKey:               os.Getenv("REGISTER_NOTIFICATION_KEY"),
+		registerNotificationExchange:          os.Getenv("REGISTER_NOTIFICATION_EXCHANGE"),
+		forgottenPasswordNotificationKey:      os.Getenv("FORGOT_PASSWORD_NOTIFICATION_KEY"),
+		forgottenPasswordNotificationQueue:    os.Getenv("FORGOT_PASSWORD_NOTIFICATION_QUEUE"),
+		forgottenPasswordNotificationExchange: os.Getenv("FORGOT_PASSWORD_NOTIFICATION_EXCHANGE"),
 	}
 }
 
@@ -34,7 +42,26 @@ func (q Queue) RegisterNotification(ctx context.Context, user *entity.User) erro
 		return err
 	}
 
-	if err = q.amqp.Producer(ctx, q.registerNotificationQueue, q.registerNotificationExchange, body); err != nil {
+	if err = q.amqp.Producer(ctx, q.registerNotificationKey, q.registerNotificationExchange, body); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (q Queue) ForgottenPasswordNotification(ctx context.Context, user *entity.User, token string) error {
+	input := map[string]string{
+		"name":  user.Name,
+		"email": user.Email,
+		"token": token,
+	}
+
+	body, err := json.Marshal(&input)
+	if err != nil {
+		return err
+	}
+
+	if err = q.amqp.Producer(ctx, q.forgottenPasswordNotificationKey, q.forgottenPasswordNotificationExchange, body); err != nil {
 		return err
 	}
 
