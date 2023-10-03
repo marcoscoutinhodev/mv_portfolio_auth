cd43890b2abd8149d42387dc0aeea907085c52ac
marcoscoutinhodev@outlook.com <marcoscoutinhodev@outlook.com>
2023-10-03 12:23:27 -0300

    feat: [auth_controller] add forgotten-password handler

--- internal/infra/http/controller/auth.go
+++ internal/infra/http/controller/auth.go
@@ -113,3 +113,43 @@ func (a Auth) SignIn(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(output.StatusCode)
 	json.NewEncoder(w).Encode(output)
 }
+
+func (a Auth) ForgottenPassword(w http.ResponseWriter, r *http.Request) {
+	var input user.ForgottenPasswordInput
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"error": err,
+		})
+		return
+	}
+
+	input.Email = strings.ToLower(strings.Join(strings.Fields(input.Email), " "))
+
+	var errors []string
+
+	if _, err := mail.ParseAddress(input.Email); err != nil {
+		errors = append(errors, "poorly formatted email")
+	}
+
+	if len(errors) > 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"error": errors,
+		})
+		return
+	}
+
+	output, err := a.usecase.ForgottenPassword(r.Context(), &input)
+	if err != nil {
+		fmt.Println("internal server error:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"error": "internal server error",
+		})
+		return
+	}
+
+	w.WriteHeader(output.StatusCode)
+	json.NewEncoder(w).Encode(output)
+}
