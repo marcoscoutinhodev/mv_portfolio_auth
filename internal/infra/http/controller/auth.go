84f8e7e7edfcb06dc60d81a0c07550776355e52b
marcoscoutinhodev@outlook.com <marcoscoutinhodev@outlook.com>
2023-10-03 11:37:30 -0300

    feat: [auth_controller] add signin handler

--- internal/infra/http/controller/auth.go
+++ internal/infra/http/controller/auth.go
@@ -59,7 +59,51 @@ func (a Auth) SignUp(w http.ResponseWriter, r *http.Request) {
 	output, err := a.usecase.Register(r.Context(), &input)
 	if err != nil {
 		fmt.Println("internal server error:", err)
-		w.WriteHeader(output.StatusCode)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"error": "internal server error",
+		})
+		return
+	}
+
+	w.WriteHeader(output.StatusCode)
+	json.NewEncoder(w).Encode(output)
+}
+
+func (a Auth) SignIn(w http.ResponseWriter, r *http.Request) {
+	var input user.AuthInput
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"error": err,
+		})
+		return
+	}
+
+	input.Email = strings.ToLower(strings.Join(strings.Fields(input.Email), " "))
+
+	var errors []string
+
+	if _, err := mail.ParseAddress(input.Email); err != nil {
+		errors = append(errors, "poorly formatted email")
+	}
+
+	if !pkg.PasswordValidator(input.Password) {
+		errors = append(errors, "poorly formatted password")
+	}
+
+	if len(errors) > 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"error": errors,
+		})
+		return
+	}
+
+	output, err := a.usecase.Auth(r.Context(), &input)
+	if err != nil {
+		fmt.Println("internal server error:", err)
+		w.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(w).Encode(map[string]interface{}{
 			"error": "internal server error",
 		})
