6d5bc85509dd0345b09be5ad3c99b7f0475ea998
marcoscoutinhodev@outlook.com <marcoscoutinhodev@outlook.com>
2023-10-15 13:35:27 -0300

    [client] add initial structure

---
+++ client/.eslintrc.js
@@ -0,0 +1,12 @@
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/eslint-config-airbnb',
+  ],
+  rules: {
+    'vuejs-accessibility/form-control-has-label': 'off',
+  },
+};
---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div>
+          <router-view />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
