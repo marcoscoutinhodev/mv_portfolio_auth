d6628d14a4f256492a066f209d5ad93187347f8e
marcoscoutinhodev@outlook.com <marcoscoutinhodev@outlook.com>
2023-10-14 21:50:47 -0300

    [ms_auth] refactoring EmailNotification

--- ms_auth/config/environment.go
+++ ms_auth/config/environment.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"net/url"
 	"os"
 	"strings"
 
@@ -8,19 +9,19 @@ import (
 )
 
 var (
-	DB_DRIVER                             string
-	DB_URI                                string
-	DB_NAME                               string
-	DB_MAX_IDLE_CONNS                     string
-	SERVER_PORT                           string
-	RBMQ_URI                              string
-	REGISTER_NOTIFICATION_KEY             string
-	REGISTER_NOTIFICATION_EXCHANGE        string
-	FORGOT_PASSWORD_NOTIFICATION_KEY      string
-	FORGOT_PASSWORD_NOTIFICATION_EXCHANGE string
-	JWT_PRIVATE_KEY                       string
-	JWT_PUBLIC_KEY                        string
-	JWT_SECRET_KEY                        string
+	REGISTER_NOTIFICATION_URL        string
+	FORGOT_PASSWORD_NOTIFICATION_URL string
+	DB_DRIVER                        string
+	DB_URI                           string
+	DB_NAME                          string
+	DB_MAX_IDLE_CONNS                string
+	SERVER_PORT                      string
+	RBMQ_URI                         string
+	EMAIL_NOTIFICATION_KEY           string
+	EMAIL_NOTIFICATION_EXCHANGE      string
+	JWT_PRIVATE_KEY                  string
+	JWT_PUBLIC_KEY                   string
+	JWT_SECRET_KEY                   string
 )
 
 func Load() {
@@ -28,6 +29,22 @@ func Load() {
 	godotenv.Load()
 
 	var errors []string
+
+	if REGISTER_NOTIFICATION_URL = os.Getenv("REGISTER_NOTIFICATION_URL"); REGISTER_NOTIFICATION_URL == "" {
+		errors = append(errors, "REGISTER_NOTIFICATION_URL not found")
+	} else {
+		if _, err := url.ParseRequestURI(REGISTER_NOTIFICATION_URL + "test"); err != nil {
+			errors = append(errors, "REGISTER_NOTIFICATION_URL has invalid format")
+		}
+	}
+
+	if FORGOT_PASSWORD_NOTIFICATION_URL = os.Getenv("FORGOT_PASSWORD_NOTIFICATION_URL"); FORGOT_PASSWORD_NOTIFICATION_URL == "" {
+		errors = append(errors, "FORGOT_PASSWORD_NOTIFICATION_URL not found")
+	} else {
+		if _, err := url.ParseRequestURI(FORGOT_PASSWORD_NOTIFICATION_URL + "test"); err != nil {
+			errors = append(errors, "FORGOT_PASSWORD_NOTIFICATION_URL has invalid format")
+		}
+	}
 
 	if DB_DRIVER = os.Getenv("DB_DRIVER"); DB_DRIVER == "" {
 		errors = append(errors, "DB_DRIVER not found")
@@ -53,20 +70,12 @@ func Load() {
 		errors = append(errors, "RBMQ_URI not found")
 	}
 
-	if REGISTER_NOTIFICATION_KEY = os.Getenv("REGISTER_NOTIFICATION_KEY"); REGISTER_NOTIFICATION_KEY == "" {
-		errors = append(errors, "REGISTER_NOTIFICATION_KEY not found")
+	if EMAIL_NOTIFICATION_KEY = os.Getenv("EMAIL_NOTIFICATION_KEY"); EMAIL_NOTIFICATION_KEY == "" {
+		errors = append(errors, "EMAIL_NOTIFICATION_KEY not found")
 	}
 
-	if REGISTER_NOTIFICATION_EXCHANGE = os.Getenv("REGISTER_NOTIFICATION_EXCHANGE"); REGISTER_NOTIFICATION_EXCHANGE == "" {
-		errors = append(errors, "REGISTER_NOTIFICATION_EXCHANGE not found")
-	}
-
-	if FORGOT_PASSWORD_NOTIFICATION_KEY = os.Getenv("FORGOT_PASSWORD_NOTIFICATION_KEY"); FORGOT_PASSWORD_NOTIFICATION_KEY == "" {
-		errors = append(errors, "FORGOT_PASSWORD_NOTIFICATION_KEY not found")
-	}
-
-	if FORGOT_PASSWORD_NOTIFICATION_EXCHANGE = os.Getenv("FORGOT_PASSWORD_NOTIFICATION_EXCHANGE"); FORGOT_PASSWORD_NOTIFICATION_EXCHANGE == "" {
-		errors = append(errors, "FORGOT_PASSWORD_NOTIFICATION_EXCHANGE not found")
+	if EMAIL_NOTIFICATION_EXCHANGE = os.Getenv("EMAIL_NOTIFICATION_EXCHANGE"); EMAIL_NOTIFICATION_EXCHANGE == "" {
+		errors = append(errors, "EMAIL_NOTIFICATION_EXCHANGE not found")
 	}
 
 	if JWT_PRIVATE_KEY = os.Getenv("JWT_PRIVATE_KEY"); JWT_PRIVATE_KEY == "" {
--- ms_auth/internal/infra/adapter/email_notification.go
+++ ms_auth/internal/infra/adapter/email_notification.go
@@ -1,8 +1,10 @@
 package adapter
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
+	"text/template"
 
 	"github.com/marcoscoutinhodev/ms_auth/config"
 	"github.com/marcoscoutinhodev/ms_auth/internal/entity"
@@ -11,29 +13,54 @@ import (
 
 type EmailNotification struct {
 	amqp                      amqp.AMQP
-	registerKey               string
-	registerExchange          string
-	forgottenPasswordKey      string
-	forgottenPasswordExchange string
+	EmailNotificationKey      string
+	EmailNotificationExchange string
 }
 
 func NewEmailNotification() *EmailNotification {
 	return &EmailNotification{
 		amqp:                      *amqp.NewAMQP(),
-		registerKey:               config.REGISTER_NOTIFICATION_KEY,
-		registerExchange:          config.REGISTER_NOTIFICATION_EXCHANGE,
-		forgottenPasswordKey:      config.FORGOT_PASSWORD_NOTIFICATION_KEY,
-		forgottenPasswordExchange: config.FORGOT_PASSWORD_NOTIFICATION_EXCHANGE,
+		EmailNotificationKey:      config.EMAIL_NOTIFICATION_KEY,
+		EmailNotificationExchange: config.EMAIL_NOTIFICATION_EXCHANGE,
 	}
 }
 
-func (q EmailNotification) producer(ctx context.Context, key, exchange string, input interface{}) error {
-	body, err := json.Marshal(&input)
+type emailNotificationPayload struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Title    string `json:"title"`
+	Template string `json:"template"`
+}
+
+func (q EmailNotification) send(ctx context.Context, user *entity.User, urlBase, token, layout, title string) error {
+	payload := struct {
+		entity.User
+		URLPath string
+	}{
+		User:    *user,
+		URLPath: urlBase + token,
+	}
+
+	var buff bytes.Buffer
+
+	t := template.Must(template.New("Template").Parse(layout))
+	if err := t.Execute(&buff, payload); err != nil {
+		return err
+	}
+
+	enp := emailNotificationPayload{
+		Name:     user.Name,
+		Email:    user.Email,
+		Title:    title,
+		Template: buff.String(),
+	}
+
+	body, err := json.Marshal(&enp)
 	if err != nil {
 		return err
 	}
 
-	if err = q.amqp.Producer(ctx, key, exchange, body); err != nil {
+	if err := q.amqp.Producer(ctx, q.EmailNotificationKey, q.EmailNotificationExchange, body); err != nil {
 		return err
 	}
 
@@ -41,13 +68,11 @@ func (q EmailNotification) producer(ctx context.Context, key, exchange string, i
 }
 
 func (q EmailNotification) Register(ctx context.Context, user *entity.User, token string) error {
-	input := map[string]string{
-		"name":  user.Name,
-		"email": user.Email,
-		"token": token,
-	}
+	layout := `	<p>Hey {{ .Name }}, welcome to MV's Portfolio</p>
+							<p>To confirm your email address <a href="{{ .URLPath }}">click here</a></p>`
+	title := "Confirm your email address"
 
-	if err := q.producer(ctx, q.registerKey, q.registerExchange, &input); err != nil {
+	if err := q.send(ctx, user, config.REGISTER_NOTIFICATION_URL, token, layout, title); err != nil {
 		return err
 	}
 
@@ -55,13 +80,12 @@ func (q EmailNotification) Register(ctx context.Context, user *entity.User, toke
 }
 
 func (q EmailNotification) ForgottenPassword(ctx context.Context, user *entity.User, token string) error {
-	input := map[string]string{
-		"name":  user.Name,
-		"email": user.Email,
-		"token": token,
-	}
+	layout := `	<p>Hey {{ .Name }}, We've received your forgotten password request.</p>
+							<p>To change your password <a href="{{ .URLPath }}">click here</a></p>
+							<p>If you didn't request it, don't worry, you can just ignore it, as long as you never share your email and password, your account will be safe</p>`
+	title := "Recover your password"
 
-	if err := q.producer(ctx, q.forgottenPasswordKey, q.forgottenPasswordExchange, &input); err != nil {
+	if err := q.send(ctx, user, config.FORGOT_PASSWORD_NOTIFICATION_URL, token, layout, title); err != nil {
 		return err
 	}
 
